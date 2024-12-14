4f6ff54bf758f0105955066d2e26a5309acb43de
jnez137 <jrnesbitt@gmail.com>
2024-12-14 10:52:33 -0800

    move SUR to submodule, partially clean up

--- src/codeV-commands.f90
+++ src/codeV-commands.f90
@@ -37,6 +37,13 @@ module codeV_commands
     end subroutine cmdImplementation 
  end interface    
 
+ interface
+ module subroutine execSUR(iptStr)
+ character(len=*) :: iptStr
+ end subroutine execSUR
+ end interface
+
+
     character(len=4), dimension(500) :: surfCmds
     type(zoa_cmd), dimension(581) :: zoaCmds
 
@@ -90,7 +97,7 @@ module codeV_commands
         zoaCmds(503)%cmd = "VIE"
         zoaCmds(503)%execFunc => execVie    
         zoaCmds(504)%cmd = "SUR"
-        zoaCmds(504)%execFunc => execSUR_new                                
+        zoaCmds(504)%execFunc => execSUR                               
         zoaCmds(505)%cmd = "STO"
         zoaCmds(505)%execFunc => execSTO    
         zoaCmds(506)%cmd = "S"
@@ -1491,201 +1498,7 @@ module codeV_commands
     end subroutine
 
 
-    subroutine execSUR_new(iptStr)
-    ! for now support SUR SA only
-    ! New code - add abstraction of row titles and new columns of RMD GLA CCY THC GLC
-                
-        use command_utils, only : parseCommandIntoTokens
-        use type_utils, only: int2str, blankStr, real2str
-        use handlers, only: updateTerminalLog
-        use global_widgets, only: curr_lens_data
-        use mod_lens_data_manager
-    
-        implicit none
 
-        !class(zoa_cmd) :: self
-        character(len=*) :: iptStr
-        integer :: ii
-        character(len=80) :: tokens(40)
-        character(len=256) :: fullLine
-        character(len=4)  :: surfTxt
-        character(len=23) :: radTxt
-        character(len=23) :: thiTxt
-        character(len=40) :: glaTxt
-        integer :: numTokens
-
-        !numSurfaces = curr_lens_data%num_surfaces
-        !call LogTermFOR("Num Surfaces is "//trim(int2str(curr_lens_data%num_surfaces)))
-
-        call parseCommandIntoTokens(trim(iptStr), tokens, numTokens, ' ')
-        if (numTokens == 2 ) then
-            if (isSurfCommand(trim(tokens(2)))) then
-                !call logTermFOR("SUR Cmd here!")
-                ! SA
-                fullLine = blankStr(10)//"RDY"//blankStr(10)//"THI"//blankStr(5)//"RMD"//blankStr(5)//"GLA" &
-                & //blankStr(10)//"CCY"//blankStr(5)//"THC"//blankStr(5)//"GLC"
-                call updateTerminalLog(trim(fullLine), "black")
-                do ii=1,curr_lens_data%num_surfaces
-                    surfTxt = blankStr(2)//trim(int2str(ii-1))//":"
-                    ! Special Cases
-                    if(ii==1)                           surfTxt = "OBJ:"
-                    if(ii==curr_lens_data%ref_stop)     surfTxt = "STO:"
-                    if(ii==curr_lens_data%num_surfaces) surfTxt = "IMG:"
-                    if (curr_lens_data%radii(ii) == 0) then
-                        radTxt = 'INFINITY'
-                    else
-                        radTxt = real2str(curr_lens_data%radii(ii),5)
-                    end if
-                    if (curr_lens_data%thicknesses(ii) > 1e10) then
-                        thiTxt = 'INFINITY'
-                    else
-                        
-                        if (curr_lens_data%thicknesses(ii) < 0) then
-                            thiTxt = real2str(curr_lens_data%thicknesses(ii),3)
-                        else
-                            thiTxt = real2str(curr_lens_data%thicknesses(ii),5)
-                        end if
-
-                    end if
-                    glaTxt = curr_lens_data%glassnames(ii)
-                    !TODO:  make this more elegant
-                    if (glaTxt(1:1).NE.' ') then
-                    !else
-                        glaTxt = trim(curr_lens_data%glassnames(ii))//'_'//trim(curr_lens_data%catalognames(ii))
-                    end if 
-                    !PRINT *, "glaTxt is ", glaTxt(1:4)
-                    !PRINT *, "glaTxt check is ", (glaTxt(1:4).EQ.'REFL')
-                    if (glaTxt(1:4).EQ.'REFL') then
-                        glaTxt = ' '
-                        fullLine = surfTxt//blankStr(4)//trim(radTxt)// &
-                        & blankStr(4)//trim(thiTxt)//blankStr(5)//'REFL' &
-                        & //blankStr(5)//trim(glaTxt)         
-                    else
-
-                        fullLine = surfTxt//blankStr(4)//trim(radTxt)// &
-                        & blankStr(4)//trim(thiTxt)//blankStr(10)// &
-                        & trim(glaTxt)
-                    end if      
-
-                    ! Codes
-                    
-                    fullLine = trim(fullLine)//blankStr(5)//ldm%getCCYCodeAsStr(ii)//blankStr(5)//ldm%getTHCCodeAsStr(ii)//blankStr(5)
-
-
-
-                    call updateTerminalLog(trim(fullLine), "black")
-
-
-                end do
-
-
-
-            else
-
-            call updateTerminalLog("SUR Should have a surface identifier (S0, Sk, Si, SA)", "red")
-            end if
-
-        else
-            call updateTerminalLog("No Surface identifier given!  Please try again", "red")
-
-        end if
-
-
-
-    end subroutine
-
-    subroutine execSUR(iptStr)
-        use command_utils, only : parseCommandIntoTokens
-        use type_utils, only: int2str, blankStr, real2str
-        use handlers, only: updateTerminalLog
-        use global_widgets, only: curr_lens_data
-    
-        implicit none
-
-        !class(zoa_cmd) :: self
-        character(len=*) :: iptStr
-        integer :: ii
-        character(len=80) :: tokens(40)
-        character(len=256) :: fullLine
-        character(len=4)  :: surfTxt
-        character(len=23) :: radTxt
-        character(len=23) :: thiTxt
-        character(len=40) :: glaTxt
-        integer :: numTokens
-
-        !numSurfaces = curr_lens_data%num_surfaces
-        !call LogTermFOR("Num Surfaces is "//trim(int2str(curr_lens_data%num_surfaces)))
-
-        call parseCommandIntoTokens(trim(iptStr), tokens, numTokens, ' ')
-        if (numTokens == 2 ) then
-            if (isSurfCommand(trim(tokens(2)))) then
-                !call logTermFOR("SUR Cmd here!")
-                ! SA
-                fullLine = blankStr(10)//"RDY"//blankStr(10)//"THI"//blankStr(5)//"RMD"//blankStr(5)//"GLA"
-                call updateTerminalLog(trim(fullLine), "black")
-                do ii=1,curr_lens_data%num_surfaces
-                    surfTxt = blankStr(2)//trim(int2str(ii-1))//":"
-                    ! Special Cases
-                    if(ii==1)                           surfTxt = "OBJ:"
-                    if(ii==curr_lens_data%ref_stop)     surfTxt = "STO:"
-                    if(ii==curr_lens_data%num_surfaces) surfTxt = "IMG:"
-                    if (curr_lens_data%radii(ii) == 0) then
-                        radTxt = 'INFINITY'
-                    else
-                        radTxt = real2str(curr_lens_data%radii(ii),5)
-                    end if
-                    if (curr_lens_data%thicknesses(ii) > 1e10) then
-                        thiTxt = 'INFINITY'
-                    else
-                        
-                        if (curr_lens_data%thicknesses(ii) < 0) then
-                            thiTxt = real2str(curr_lens_data%thicknesses(ii),3)
-                        else
-                            thiTxt = real2str(curr_lens_data%thicknesses(ii),5)
-                        end if
-
-                    end if
-                    glaTxt = curr_lens_data%glassnames(ii)
-                    !TODO:  make this more elegant
-                    if (glaTxt(1:1).NE.' ') then
-                    !else
-                        glaTxt = trim(curr_lens_data%glassnames(ii))//'_'//trim(curr_lens_data%catalognames(ii))
-                    end if 
-                    PRINT *, "glaTxt is ", glaTxt(1:4)
-                    PRINT *, "glaTxt check is ", (glaTxt(1:4).EQ.'REFL')
-                    if (glaTxt(1:4).EQ.'REFL') then
-                        glaTxt = ' '
-                        fullLine = surfTxt//blankStr(4)//trim(radTxt)// &
-                        & blankStr(4)//trim(thiTxt)//blankStr(5)//'REFL' &
-                        & //blankStr(5)//trim(glaTxt)         
-                    else
-
-                        fullLine = surfTxt//blankStr(4)//trim(radTxt)// &
-                        & blankStr(4)//trim(thiTxt)//blankStr(10)// &
-                        & trim(glaTxt)
-                    end if                                       
-
-
-                    call updateTerminalLog(trim(fullLine), "black")
-
-
-                end do
-
-
-
-            else
-
-            call updateTerminalLog("SUR Should have a surface identifier (S0, Sk, Si, SA)", "red")
-            end if
-
-        else
-            call updateTerminalLog("No Surface identifier given!  Please try again", "red")
-
-        end if
-
-
-
-    end subroutine
 
     subroutine execRestore(iptStr)
         !use global_widgets, only: curr_lens_data
