99b87f590effa42ee1490b1aa5ab0cdf6eb7d084
jnez137 <jrnesbitt@gmail.com>
2024-12-26 18:00:32 -0800

    move some plot cmds to plot submodule

--- src/codeV-commands.f90
+++ src/codeV-commands.f90
@@ -55,6 +55,15 @@ end subroutine setPlotZernikeCoefficients
 module subroutine ZERN_TST(iptStr)
 character(len=*) :: iptStr
 end subroutine ZERN_TST
+module subroutine execVie(iptStr)
+character(len=*) :: iptStr
+end subroutine execVie
+module subroutine execRMSPlot(iptStr)
+character(len=*) :: iptStr
+end subroutine execRMSPlot 
+module subroutine execSeidelBarChart(iptStr)
+character(len=*) :: iptStr
+end subroutine execSeidelBarChart    
  end interface
 
 
@@ -897,46 +906,6 @@ end subroutine ZERN_TST
 
     end function
 
-    ! psuedocode for checking plot input
-    ! if user entered PX
-    !    check if PX plot exists already
-    !    if no, create new psm
-    ! if user did not enter PX
-    !   create new default settings
-    ! in all cases update cmd_loop to plot loop
-
-    subroutine execVIE(iptStr)
-        !use ui_spot, only: spot_struct_settings, spot_settings
-       ! use mod_plotopticalsystem
-
-        implicit none
-        character(len=*) :: iptStr
-        type(zoaplot_setting_manager) :: psm
-
-        character(len=80) :: tokens(40)
-        integer :: numTokens
-        logical :: plotExists
-
-        call parse(trim(iptStr), ' ', tokens, numTokens) 
-
-        call psm%initialize(trim(iptStr))
-        cmd_loop = VIE_LOOP
-
-        !call LogTermFOR("About ot check VIE for existing plot")
-        if (numTokens  == 2) then
-            plotExists = checkForExistingPlot(tokens(1:2), psm, ID_PLOTTYPE_LENSDRAW)
-            !ßcall LogTermFOR("VIE plot exists is "//bool2str(plotExists))
-            ! If plotExiss then curr_psm is sst so we are good.  Seems like a bad design
-            ! here but don't have a better soultion right now
-           if (plotExists) return
-        end if
-        call psm%addLensDrawSettings()
-        ! Set up settings
-        !call psm%addWavelengthSetting()
-        !call psm%addDensitySetting(10, 8, 21)
-        curr_psm = psm
-    end subroutine
-
     function initiatePlotLoop(iptStr, PLOT_CODE, psm) result(boolResult)
 
         implicit none
@@ -1017,43 +986,6 @@ end subroutine ZERN_TST
 
     end subroutine
 
-    subroutine  execRMSPlot(iptStr)
-        implicit none
-        character(len=*) :: iptStr
-        logical :: boolResult
-        type(zoaplot_setting_manager) :: psm
-
-
-        call psm%initialize(trim(iptStr))
-        !call psm%addDensitySetting(64,8,128)
-        !1call psm%addFieldSetting()
-        call psm%addRMSFieldSettings()
-
-        boolResult = initiatePlotLoop(iptStr, ID_PLOTTYPE_RMSFIELD, psm)
-        if(boolResult .EQV. .FALSE.) then
-            call updateTerminalLog("Error in input. Should be either RIM or RIM PX, where X is plot num", "red")
-        end if
-
-    end subroutine
-
-    subroutine  execSeidelBarChart(iptStr)
-        implicit none
-        character(len=*) :: iptStr
-        logical :: boolResult
-        type(zoaplot_setting_manager) :: psm
-
-
-        call psm%initialize(trim(iptStr))
-        !call psm%addDensitySetting(64,8,128)
-        !1call psm%addFieldSetting()
-        call psm%addWavelengthSetting()
-
-        boolResult = initiatePlotLoop(iptStr, ID_PLOTTYPE_SEIDEL, psm)
-        if(boolResult .EQV. .FALSE.) then
-            call updateTerminalLog("Error in input. Should be either RIM or RIM PX, where X is plot num", "red")
-        end if
-
-    end subroutine
 
     subroutine execRayAberrationPlot(iptStr)
         implicit none
--- src/codeV-plot.f90
+++ src/codeV-plot.f90
@@ -102,4 +102,81 @@ contains
 
 
     end procedure 
+
+
+    ! psuedocode for checking plot input
+    ! if user entered PX
+    !    check if PX plot exists already
+    !    if no, create new psm
+    ! if user did not enter PX
+    !   create new default settings
+    ! in all cases update cmd_loop to plot loop
+
+    module procedure execVIE
+        !use ui_spot, only: spot_struct_settings, spot_settings
+       ! use mod_plotopticalsystem
+
+        implicit none
+        type(zoaplot_setting_manager) :: psm
+
+        character(len=80) :: tokens(40)
+        integer :: numTokens
+        logical :: plotExists
+
+        call parse(trim(iptStr), ' ', tokens, numTokens) 
+
+        call psm%initialize(trim(iptStr))
+        cmd_loop = VIE_LOOP
+
+        !call LogTermFOR("About ot check VIE for existing plot")
+        if (numTokens  == 2) then
+            plotExists = checkForExistingPlot(tokens(1:2), psm, ID_PLOTTYPE_LENSDRAW)
+            !ßcall LogTermFOR("VIE plot exists is "//bool2str(plotExists))
+            ! If plotExiss then curr_psm is sst so we are good.  Seems like a bad design
+            ! here but don't have a better soultion right now
+           if (plotExists) return
+        end if
+        call psm%addLensDrawSettings()
+        ! Set up settings
+        !call psm%addWavelengthSetting()
+        !call psm%addDensitySetting(10, 8, 21)
+        curr_psm = psm
+    end procedure  
+
+    module procedure execRMSPlot
+        implicit none
+
+        logical :: boolResult
+        type(zoaplot_setting_manager) :: psm
+
+
+        call psm%initialize(trim(iptStr))
+        !call psm%addDensitySetting(64,8,128)
+        !1call psm%addFieldSetting()
+        call psm%addRMSFieldSettings()
+
+        boolResult = initiatePlotLoop(iptStr, ID_PLOTTYPE_RMSFIELD, psm)
+        if(boolResult .EQV. .FALSE.) then
+            call updateTerminalLog("Error in input. Should be either RIM or RIM PX, where X is plot num", "red")
+        end if
+
+    end procedure    
+
+    module procedure execSeidelBarChart
+        implicit none
+        logical :: boolResult
+        type(zoaplot_setting_manager) :: psm
+
+
+        call psm%initialize(trim(iptStr))
+        !call psm%addDensitySetting(64,8,128)
+        !1call psm%addFieldSetting()
+        call psm%addWavelengthSetting()
+
+        boolResult = initiatePlotLoop(iptStr, ID_PLOTTYPE_SEIDEL, psm)
+        if(boolResult .EQV. .FALSE.) then
+            call updateTerminalLog("Error in input. Should be either RIM or RIM PX, where X is plot num", "red")
+        end if
+
+    end procedure    
 end submodule
